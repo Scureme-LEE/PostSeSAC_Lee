46b5b5d58b6276907a53be2f222cc3c27c6b1ee6
ldh <erichl5000@gmail.com>
2025-01-13 23:17:11 +0900

    화면과 기능 구성한 Counter 컴포넌트 생성

---
+++ vue-counter/src/components/counter.vue
@@ -0,0 +1,43 @@
+<template>
+    <div style="text-align:center; position:relative">
+        <h1>계산기</h1>
+        <div style="display: flex; align-items: center; justify-content: center; gap: 10px;">
+        <button @mousedown="startDecrement" @mouseup="stop">-</button>
+        <h2> {{ counter }}</h2>
+        <button @mousedown="startIncrement" @mouseup="stop">+</button>
+        </div>
+        <button @click="reset" style="position:absolute;margin-top:30px;right:30%;">Reset</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'CounterComponent',
+        data(){
+            return {
+                counter:0, intervalID: null,
+                };
+            },
+        methods:{
+            increment(){
+                this.counter++;
+            },
+            decrement(){
+                this.counter--;
+            },
+            startIncrement(){
+                this.intervalID=setInterval(() => this.increment(), 40);
+            },
+            startDecrement(){
+                this.intervalID=setInterval(() => this.decrement(), 40);
+            },
+            stop(){
+                clearInterval(this.intervalID);
+            },
+            reset(){
+                this.counter=0;
+            }
+        },
+
+    };
+</script>
