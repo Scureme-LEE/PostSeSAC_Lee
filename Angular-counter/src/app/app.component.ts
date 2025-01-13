0eeb907bbadf410f79348b61e65ff7507eb27441
ldh <erichl5000@gmail.com>
2025-01-13 18:28:27 +0900

    계산기 컴포넌트 넣기기

--- Angular-counter/src/app/app.component.ts
+++ Angular-counter/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CounterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true
 })
 export class AppComponent {
   title = 'Angular-counter';
