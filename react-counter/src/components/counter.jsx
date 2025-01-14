2437a228549f42465e17147efaafb626c579ba82
ldh <erichl5000@gmail.com>
2025-01-14 18:06:07 +0900

    기본적 counter 기능 삽입

--- react-counter/src/components/counter.jsx
+++ react-counter/src/components/counter.jsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
 const Counter=()=>{
+    const [counter, setCounter]=useState(0);
+    const increment=()=>setCounter(counter+1);
+    const decrement=()=>setCounter(counter-1);
+
     return (
         <>
         <h1>카운터</h1>
-        <button>-</button>
-        <h2>{{counter}}</h2>
-        <button>+</button>        
+        <button onClick={decrement}>-</button>
+        <h2>{counter}</h2>
+        <button onClick={increment}>+</button>        
         </>
     );
 };
