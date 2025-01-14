a69280a63c1d8603e06098ab3f034e610850b03d
ldh <erichl5000@gmail.com>
2025-01-14 19:20:54 +0900

    스타일 추가

--- react-counter/src/components/counter.jsx
+++ react-counter/src/components/counter.jsx
@@ -23,13 +23,15 @@ const Counter=()=>{
     const reset=()=>setCounter(0);
 
     return (
-        <>
-        <h1>카운터</h1>
-        <button onMouseDown={decrement} onMouseUp={stop}>-</button>
-        <h2>{counter}</h2>
-        <button onMouseDown={increment} onMouseUp={stop}>+</button> 
-        <button onClick={reset}>리셋</button>      
-        </>
+        <div style={{textAlign:'center',position:'realative'}}>
+            <h1>카운터</h1>
+            <div style={{display: 'flex', alignItems:'center', justifyContent:'center', gap:'10px'}}>
+                <button onMouseDown={decrement} onMouseUp={stop}>-</button>
+                <h2>{counter}</h2>
+                <button onMouseDown={increment} onMouseUp={stop}>+</button> 
+            </div>
+            <button onClick={reset} style={{position:'absolute',marginTop:'30px',right:'30%'}}>리셋</button>      
+        </div>
     );
 };
 
