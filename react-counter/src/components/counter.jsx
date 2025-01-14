48d8778e12c80694a4eef88f225251d45eac2103
ldh <erichl5000@gmail.com>
2025-01-14 18:22:59 +0900

    리셋 버튼 추가

--- react-counter/src/components/counter.jsx
+++ react-counter/src/components/counter.jsx
@@ -4,13 +4,14 @@ const Counter=()=>{
     const [counter, setCounter]=useState(0);
     const increment=()=>setCounter(counter+1);
     const decrement=()=>setCounter(counter-1);
-
+    const reset=()=>setCounter(0)
     return (
         <>
         <h1>카운터</h1>
         <button onClick={decrement}>-</button>
         <h2>{counter}</h2>
-        <button onClick={increment}>+</button>        
+        <button onClick={increment}>+</button> 
+        <button onClick={reset}>리셋</button>      
         </>
     );
 };
